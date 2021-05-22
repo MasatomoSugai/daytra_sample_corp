d6ae8a9bfc7012409ea267d86121d0c5c9277571
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-05-22 22:55:51 +0900

    swiper

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -20,4 +20,34 @@ const swiper = new Swiper('.swiper-container', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  
+
+
+
+
+
+
+
 });
